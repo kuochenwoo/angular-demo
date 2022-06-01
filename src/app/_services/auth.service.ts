a6c0da26f8cba9aed5c7a5f7bee8ef8d644b282d
kuochenwoo <kuochenwoo@live.com>
2022-06-01 10:50:31 +0800

    change register api

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -35,7 +35,7 @@ export class AuthService {
   register(username: string, email: string, password: string): Observable<any> {
 
     return this.http.post(
-      AUTH_API + 'signup',
+      AUTH_API + 'register',
       {
         username,
         email,
